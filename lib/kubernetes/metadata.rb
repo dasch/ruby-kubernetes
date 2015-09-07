dce215aef571e2b112f2d20869cb3a99effb31ae
Daniel Schierbeck <dasch@zendesk.com>
2015-09-07 14:11:54 +0200

    Labels and annotations are maps, not lists

--- lib/kubernetes/metadata.rb
+++ lib/kubernetes/metadata.rb
@@ -12,8 +12,8 @@ module Kubernetes
       @resource_version = data.fetch("resourceVersion")
       @creation_timestamp = Time.iso8601(data.fetch("creationTimestamp"))
       @generate_name = data.fetch("generateName", nil)
-      @labels = data.fetch("labels", [])
-      @annotations = data.fetch("annotations", [])
+      @labels = data.fetch("labels", {})
+      @annotations = data.fetch("annotations", {})
     end
   end
 end
