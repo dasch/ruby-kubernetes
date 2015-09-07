2667e2519f9148f538709815695aef9a226d2c33
Daniel Schierbeck <dasch@zendesk.com>
2015-09-07 14:00:11 +0200

    Allow listing pods

--- lib/kubernetes/client.rb
+++ lib/kubernetes/client.rb
@@ -16,6 +16,74 @@ module Kubernetes
       data.fetch("items").map {|item| Namespace.new(item) }
     end
 
+    def get_pods
+      response = @connection.get(path: "/api/v1/namespaces/default/pods")
+      data = JSON.parse(response.body)
+      data.fetch("items").map {|item| Pod.new(item) }
+    end
+
+    class Pod
+      attr_reader :metadata, :spec, :status
+
+      def initialize(data)
+        @metadata = Metadata.new(data.fetch("metadata"))
+        @spec = PodSpec.new(data.fetch("spec"))
+        @status = PodStatus.new(data.fetch("status"))
+      end
+    end
+
+    class PodSpec
+      def initialize(data)
+        @containers = data.fetch("containers", []).map {|item|
+          Container.new(item)
+        }
+
+        @restart_policy = data.fetch("restartPolicy")
+      end
+    end
+
+    class PodStatus
+      def initialize(data)
+        @phase = data.fetch("phase")
+        @message = data.fetch("message", nil)
+        @reason = data.fetch("reason", nil)
+        @host_ip = data.fetch("hostIP", nil)
+        @pod_ip = data.fetch("podIP", nil)
+        @start_time = Time.iso8601(data.fetch("startTime"))
+      end
+
+      def pending?
+        @phase == "Pending"
+      end
+
+      def running?
+        @phase == "Running"
+      end
+
+      def succeeded?
+        @phase == "Succeeded"
+      end
+
+      def failed?
+        @phase == "Failed"
+      end
+    end
+
+    class Container
+      attr_reader :name, :image, :command, :args, :working_dir
+      attr_reader :image_pull_policy, :termination_message_path
+
+      def initialize(data)
+        @name = data.fetch("name")
+        @image = data.fetch("image")
+        @command = data.fetch("command", [])
+        @args = data.fetch("args", [])
+        @working_dir = data.fetch("workingDir", nil)
+        @image_pull_policy = data.fetch("imagePullPolicy")
+        @termination_message_path = data.fetch("terminationMessagePath")
+      end
+    end
+
     class Namespace
       attr_reader :metadata, :spec, :status
 
@@ -34,9 +102,15 @@ module Kubernetes
       attr_reader :name, :link, :uid, :resource_version, :creation_timestamp
 
       def initialize(data)
-        @name, @link, @uid = data.values_at("name", "selfLink", "uid")
+        @name = data.fetch("name")
+        @namespace = data.fetch("namespace", nil)
+        @link = data.fetch("selfLink")
+        @uid = data.fetch("uid")
         @resource_version = data.fetch("resourceVersion")
         @creation_timestamp = Time.iso8601(data.fetch("creationTimestamp"))
+        @generate_name = data.fetch("generateName", nil)
+        @labels = data.fetch("labels", [])
+        @annotations = data.fetch("annotations", [])
       end
     end
 
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 describe Kubernetes::Client do
+  let(:client) { Kubernetes::Client.new }
+
   describe "#get_namespaces" do
     it "lists the available namespaces" do
-      client = Kubernetes::Client.new
       namespaces = client.get_namespaces
 
       expect(namespaces.map(&:name)).to include "default"
+    end
+  end
+
+  describe "#get_pods" do
+    it "lists all pods in the namespace" do
+      pods = client.get_pods
+
+      expect(pods.count).to be > 0
     end
   end
 end
